6a66ee7d737e751363f1aa9a5e8b3f5a25459301
Wangh688 <1337326302@qq.com>
2025-04-08 19:46:59 +0800

    测试

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 08 Apr 2025 07:45:01 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    cout << a << endl;
+
+    return 0;
+}
